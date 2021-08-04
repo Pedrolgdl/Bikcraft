dd70e620780fd9cf4b5a25f98a940381b9e6bbdf
Pedrolgdl <pedrolucas.guerra28@gmail.com>
2021-08-04 16:34:54 -0300

    Adicionando pasta contendo os arquivos JS necessários para fazer o slide

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+if(window.SimpleSlide) {
+  
+  new SimpleSlide({
+    slide: "quote", // nome do atributo data-slide="nome"
+    //nav: true, // se deve ou não mostrar a navegação
+    //auto: true, // se o slide deve passar automaticamente // por padrão já é true
+    time: 5000 // tempo de transição dos slides
+  });
+
+  new SimpleSlide({
+    slide: "portifolio",
+    time: 5000,
+    nav: true
+  });
+}
